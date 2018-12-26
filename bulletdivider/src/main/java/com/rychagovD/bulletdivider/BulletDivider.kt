89bf1afc3118f4f1cc98a512c7a4ac63ff513303
Rychagov Dmitry rychagov@mercurydevelopment.com <rychagov@mercurydevelopment.com>
2018-12-26 19:25:35 +0400

    Add alignment support

--- bulletdivider/src/main/java/com/rychagovD/bulletdivider/BulletDivider.kt
+++ bulletdivider/src/main/java/com/rychagovD/bulletdivider/BulletDivider.kt
@@ -27,15 +27,22 @@ class BulletDivider(
   private val DEFAULT_SPACE = 40.toPx()
   private val DEFAULT_COUNT = 3
 
+  private val ALIGNMENT_LEFT = 0
+  private val ALIGNMENT_CENTER = 1
+  private val ALIGNMENT_RIGHT = 2
+
   private val tint: Int
   private val bulletHeight: Int
   private val bulletRadius: Int
+  private val bulletAlignment: Int
   private val bulletBigWidth: Int
   private val bulletBigDistance: Int
   private val bulletBigCount: Int
   private val bulletSmallWidth: Int
   private val bulletSmallDistance: Int
   private val bulletSmallCount: Int
+
+  private val bulletAlignmentDx: Float
 
   private val bulletPaint: Paint
   private val bulletBigPath: Path
@@ -47,6 +54,7 @@ class BulletDivider(
     tint = typedArray.getColor(R.styleable.BulletDivider_android_tint, Color.BLACK)
     bulletHeight = typedArray.getDimensionPixelSize(R.styleable.BulletDivider_bullet_height, DEFAULT_HEIGHT)
     bulletRadius = bulletHeight / 2
+    bulletAlignment = typedArray.getInt(R.styleable.BulletDivider_bullet_alignment, ALIGNMENT_CENTER)
     bulletBigWidth = typedArray.getDimensionPixelSize(R.styleable.BulletDivider_bullet_big_width, DEFAULT_WIDTH_BIG)
     bulletBigCount = typedArray.getInt(R.styleable.BulletDivider_bullet_big_count, DEFAULT_COUNT)
     bulletBigDistance = typedArray.getDimensionPixelSize(R.styleable.BulletDivider_bullet_distance, DEFAULT_SPACE) - bulletHeight
@@ -54,6 +62,12 @@ class BulletDivider(
     bulletSmallCount = typedArray.getInt(R.styleable.BulletDivider_bullet_small_count, DEFAULT_COUNT)
     bulletSmallDistance = computeSmallDistance()
     typedArray.recycle()
+
+    bulletAlignmentDx = when (bulletAlignment) {
+      ALIGNMENT_LEFT -> 0f
+      ALIGNMENT_RIGHT -> (bulletBigWidth - bulletSmallWidth).toFloat()
+      else -> (bulletBigWidth - bulletSmallWidth) / 2f
+    }
 
     bulletPaint = Paint(Paint.ANTI_ALIAS_FLAG)
       .apply {
@@ -84,7 +98,6 @@ class BulletDivider(
     val bigDistance = bulletBigDistance.toFloat()
     val smallDistance = bulletSmallDistance.toFloat()
     val height = bulletHeight.toFloat()
-    val dx = (bulletBigWidth - bulletSmallWidth) / 2f
 
     canvas.save()
     repeat(bulletBigCount) {
@@ -95,7 +108,7 @@ class BulletDivider(
 
     repeat(bulletBigCount - 1) { bigIndex ->
       canvas.save()
-      canvas.translate(dx, 0f)
+      canvas.translate(bulletAlignmentDx, 0f)
       canvas.translate(0f, bigIndex * bigDistance)
       canvas.translate(0f, (bigIndex + 1) * height)
 
